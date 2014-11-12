2a4e1308b5b7abe59e1cd6a7f108b1c63267894d
Hosgel, Hasan <hasan.hosgel@immobilienscout24.de>
2014-11-12 10:40:56 +0100

    first creation

---
+++ tv/src/androidTest/java/devfestist14/android/alosdev/de/devfestist14/ApplicationTest.java
@@ -0,0 +1,13 @@
+package devfestist14.android.alosdev.de.devfestist14;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+  public ApplicationTest() {
+    super(Application.class);
+  }
+}
